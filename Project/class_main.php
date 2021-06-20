a95ac263a97ba113e04c636dd2337be9a6e6bd10
nohhow <xksrma97@gmail.com>
2021-06-21 02:39:29 +0900

    오류수정

--- Project/class_main.php
+++ Project/class_main.php
@@ -1,3 +1,42 @@
+<style>
+/* style 코드 출처 : https://codesandbox.io/u/sunhwa508 */
+#info {
+  text-transform: capitalize;
+  font-size: 3rem;
+  white-space: nowrap;
+  color: transparent;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+#info::before {
+  content: "클래스를 생성하고 '모두의 클래스'를 시작해보세요!";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  color: #da4747;
+  overflow: hidden;
+  border-right: 3px solid black;
+  animation: typing 5s steps(15) infinite;
+}
+@keyframes typing {
+  0% {
+    width: 0%;
+  }
+  50% {
+    width: 100%;
+  }
+  100% {
+    width: 0%;
+  }
+}
+
+</style>
+
 <div id="layoutSidenav">
     <div id="layoutSidenav_nav_ver2">
     </div>
@@ -5,7 +44,7 @@
         <main>
          <div class = "row">
                 <div>
-                    <button style="box-shadow:0 0.5px 3px rgba(0, 0, 0, 0.5);" id="class_btn" onclick="access1()">CREATE</button>
+                    <button style="box-shadow:0 0.5px 3px rgba(0, 0, 0, 0.5);" id="class_btn" onclick="access1()">클래스 생성</button>
                     <script>
                         // 로그인 안되어있으면 login_form.php가 보여짐
 
@@ -21,7 +60,7 @@
                         }
                     </script>
                     &nbsp;&nbsp;
-                    <button style="box-shadow:0 0.5px 3px rgba(0, 0, 0, 0.5);" id="class_btn" onclick="access2()">JOIN</button>
+                    <button style="box-shadow:0 0.5px 3px rgba(0, 0, 0, 0.5);" id="class_btn" onclick="access2()">클래스 참가</button>
                     <script>
                         // 로그인 안되어있으면 login_form.php가 보여짐
 
@@ -45,24 +84,30 @@
                 $sql = "select class_code from mem_class where mem_id='$userid'";
                 $result = mysqli_query($con, $sql) or die;
 
-                echo '<div class="row">';
+                if($num_record = mysqli_num_rows($result)){
+                
+                    echo '<div class="row">';
 
-                while ($list = mysqli_fetch_array($result)) {
-                    $sql2 = 'select * from classes where class_code="' . $list['class_code'] . '"';
-                    $class_result = mysqli_query($con, $sql2);
-                    while ($class_list = mysqli_fetch_array($class_result)) {
-                        echo '<div class="col-xl-3 col-md-6">
-                                    <div class="card bg-white text-body mb-4" style="box-shadow:0 0.5px 3px rgba(0, 0, 0, 0.3);">
-                                        <div class="card-body">' . $class_list['class_name'] . '</div>
-                                        <div class="card-footer d-flex align-items-center justify-content-between">
-                                            <a class="small text-body stretched-link" href="class_detail.php?w=notice&class_name=' . $class_list['class_name'] . '&class_info=' . $class_list['class_info'] . '&class_code=' . $class_list['class_code'] . '">'.$class_list['class_info'].'</a>
-                                            <div class="small text-body"><i class="fas fa-angle-right"></i></div>
+                    while ($list = mysqli_fetch_array($result)) {
+                        $sql2 = 'select * from classes where class_code="' . $list['class_code'] . '"';
+                        $class_result = mysqli_query($con, $sql2);
+                        while ($class_list = mysqli_fetch_array($class_result)) {
+                            echo '<div class="col-xl-3 col-md-6">
+                                        <div class="card bg-white text-body mb-4" style="box-shadow:0 0.5px 3px rgba(0, 0, 0, 0.3);">
+                                            <div class="card-body">' . $class_list['class_name'] . '</div>
+                                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                                <a class="small text-body stretched-link" href="class_detail.php?w=notice&class_name=' . $class_list['class_name'] . '&class_info=' . $class_list['class_info'] . '&class_code=' . $class_list['class_code'] . '">'.$class_list['class_info'].'</a>
+                                                <div class="small text-body"><i class="fas fa-angle-right"></i></div>
+                                            </div>
                                         </div>
-                                    </div>
-                              </div>';
+                                </div>';
+                        }
                     }
+                    echo '</div>';
+                }else{
+                    echo "<div id='info'>클래스를 생성하고 '모두의 클래스'를 시작해보세요!</div>";
                 }
-                echo '</div>';
+
                 ?>
         <main>
     </div>
